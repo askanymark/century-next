9aedda95fe5fb74e36ca03d7e71a2e92e20a407a
Marks Paskannijs <askanymark@pop-os.localdomain>
2020-06-28 17:33:50 +0100

    feat: hide desktop menu on scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,14 @@
 import smoothscroll from 'smoothscroll-polyfill';
 
 smoothscroll.polyfill();
+
+let prevScrollpos = window.pageYOffset;
+window.onscroll = () => {
+  const currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById('navbar').style.top = '0';
+  } else {
+    document.getElementById('navbar').style.top = '-150px';
+  }
+  prevScrollpos = currentScrollPos;
+};
