6663028f0b7bc4ae577af8d7c2bc2dd90bbc6163
Marks Paskannijs <askanymark@pop-os.localdomain>
2020-07-05 20:25:30 +0100

    feat: add a different menu; WIP

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,3 @@
 import smoothscroll from 'smoothscroll-polyfill';
 
 smoothscroll.polyfill();
-
-let prevScrollpos = window.pageYOffset;
-window.onscroll = () => {
-  const currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById('navbar').style.top = '0';
-  } else {
-    document.getElementById('navbar').style.top = '-150px';
-  }
-  prevScrollpos = currentScrollPos;
-};
