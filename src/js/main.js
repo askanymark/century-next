192724ae940f986590b8a81937f25a1b09adbe4a
Marks Paskannijs <goat@Markss-MacBook-Pro.local>
2020-07-12 20:22:22 +0100

    fix: overscroll behaviour on bounce effect

--- src/js/main.js
+++ src/js/main.js
@@ -3,12 +3,15 @@ import smoothscroll from 'smoothscroll-polyfill';
 smoothscroll.polyfill();
 
 let prevScrollpos = window.pageYOffset;
-window.onscroll = () => {
+window.addEventListener('scroll', (event) => {
+  event.preventDefault();
   const currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById('navbar').style.top = '0';
-  } else {
-    document.getElementById('navbar').style.top = '-150px';
+  if (currentScrollPos > 1) {
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById('navbar').style.top = '0';
+    } else {
+      document.getElementById('navbar').style.top = '-150px';
+    }
   }
   prevScrollpos = currentScrollPos;
-};
+});
